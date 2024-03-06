8b19f126ed5f0075dfdac61e3f8a7ba4bd144527
Narutchai <koonfluk2015@gmail.com>
2024-03-06 23:53:25 +0700

    test deploy

---
+++ prisma/migrations/20240306162456_test_config/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Test` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Test";
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 
 export default function Home() {
+  const [message, setMessage] = useState([]);
+
+
+  const getMessages = async () => {
+    const response = await axios.get("/api/test");
+    setMessage(response.data.message);
+  };
+
+  useEffect(() => {
+    getMessages();    
+  }, []);
+
+console.log(message);
+
   return (
     <div>
-      <h1>hello</h1>
-      <h1>hello</h1>
+      <h1>{message}</h1>
     </div>
   );
 }
